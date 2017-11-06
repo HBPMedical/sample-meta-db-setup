3e164dd158244c4a0bcb4b9f5038c9389b79174e
Ludovic Claude <ludovic.claude54@gmail.com>
2017-11-06 12:29:03 +0100

    Update tests

--- tests/sql/testData.sql
+++ tests/sql/testData.sql
@@ -1,19 +1,19 @@
 BEGIN;
 
 -- Plan the tests
-SELECT plan( 2 );
+SELECT plan( 4 );
 
 SELECT is(source::VARCHAR, 'sample', 'sample variables should be present')
-  FROM meta_variables where source = "sample";
+  FROM meta_variables where source = 'sample';
 
 SELECT is(target_table::VARCHAR, 'SAMPLE_DATA', 'Target sample_data table should be present')
-  FROM meta_variables where source = "sample";
+  FROM meta_variables where source = 'sample';
 
 SELECT is(source::VARCHAR, 'churn', 'churn variables should be present')
-  FROM meta_variables where source = "churn";
+  FROM meta_variables where source = 'churn';
 
 SELECT is(target_table::VARCHAR, 'CHURN', 'Target churn table should be present')
-  FROM meta_variables where source = "churn";
+  FROM meta_variables where source = 'churn';
 
 -- Clean up
 SELECT * FROM finish();
